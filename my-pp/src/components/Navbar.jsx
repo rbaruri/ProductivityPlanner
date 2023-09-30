baf38651ba7b29eda85af5c0cff410025dc3c2cd
Rajasree Baruri <rbaruri2002@gmail.com>
2023-09-30 11:52:32 +0530

    Sidebar

--- my-pp/src/components/Navbar.jsx
+++ my-pp/src/components/Navbar.jsx
@@ -2,22 +2,13 @@ import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 
 const Navbar = () => {
-  // Create a state variable to track the visibility of the Sidebar
-  const [sidebarVisible, setSidebarVisible] = useState(false);
-
-  // Function to toggle the Sidebar visibility
-  const toggleSidebar = () => {
-    setSidebarVisible(!sidebarVisible);
-  };
-
   return (
     <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        {/* Render the Sidebar component here and pass the visibility state */}
-        <Sidebar visible={sidebarVisible} />
-      </div>
       <div className="navbar-center">
-        {/* Add a button to toggle the Sidebar */}
+        <a className="btn btn-ghost normal-case text-xl">DEVELOG</a>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
@@ -29,9 +20,27 @@ const Navbar = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h7"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
           </svg>
+        </button>
+        <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </label>
+      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
       </div>
     </div>
   );
--- my-pp/src/components/Sidebar.jsx
+++ my-pp/src/components/Sidebar.jsx
@@ -4,8 +4,9 @@ const Sidebar = () => {
   return (
     <aside className="fixed top-0 left-0 h-full w-60 overflow-y-auto bg-opacity-80 backdrop-blur-lg bg-gray-100 text-gray-800 dark:bg-opacity-60 dark:bg-black dark:text-white p-4 space-y-4">
       <div className="text-center">
-        <h1 className="text-2xl font-semibold">Your Brand</h1>
+        <h1 className="text-2xl font-semibold">*Logo*</h1>
       </div>
+      <br></br>
       <ul className="space-y-2">
         <li>
           <a href="#" className="hover:text-blue-600">
@@ -27,42 +28,4 @@ const Sidebar = () => {
   );
 };
 
-const Navbar = () => {
-  return (
-    <div className="bg-blue-600 py-2">
-      <div className="container mx-auto flex items-center justify-between">
-        <h1 className="text-white text-xl font-semibold">Your Brand</h1>
-        <div className="hidden lg:flex space-x-4">
-          <a href="#" className="text-white hover:text-blue-200">
-            Category 1
-          </a>
-          <a href="#" className="text-white hover:text-blue-200">
-            Category 2
-          </a>
-          <a href="#" className="text-white hover:text-blue-200">
-            Category 3
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-function App() {
-  return (
-    <div className="flex h-screen bg-gray-200 dark:bg-gray-900">
-      {/* Sidebar */}
-      <Sidebar />
-      {/* Main Content */}
-      <div className="flex-1">
-        <Navbar />
-        <div className="container mx-auto p-4">
-          {/* Content Goes Here */}
-          <p>Your page content goes here.</p>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default App;
+export default Sidebar;
