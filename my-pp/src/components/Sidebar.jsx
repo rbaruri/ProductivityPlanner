bd6d58e16981fcecae9ee8b28bfc8c88cb93e505
Rajasree Baruri <rbaruri2002@gmail.com>
2023-09-30 11:34:06 +0530

    Sidebar

--- my-pp/src/components/Sidebar.jsx
+++ my-pp/src/components/Sidebar.jsx
@@ -1,24 +1,68 @@
 import React from "react";
 
-const Sidebar = ({ visible }) => {
-  // Conditionally apply classes based on the visibility prop
-  const sidebarClasses = `fixed left-0 top-0 z-[1035] h-screen w-60 ${
-    visible ? "translate-x-0" : "-translate-x-full"
-  } overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 dark:bg-zinc-800`;
-
+const Sidebar = () => {
   return (
-    <>
-      {/* Rest of your Sidebar component code */}
-      <nav
-        id="sidenav-2"
-        className={sidebarClasses}
-        // ...
-      >
-        {/* ... */}
-      </nav>
-      {/* ... */}
-    </>
+    <aside className="fixed top-0 left-0 h-full w-60 overflow-y-auto bg-opacity-80 backdrop-blur-lg bg-gray-100 text-gray-800 dark:bg-opacity-60 dark:bg-black dark:text-white p-4 space-y-4">
+      <div className="text-center">
+        <h1 className="text-2xl font-semibold">Your Brand</h1>
+      </div>
+      <ul className="space-y-2">
+        <li>
+          <a href="#" className="hover:text-blue-600">
+            Category 1
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-blue-600">
+            Category 2
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-blue-600">
+            Category 3
+          </a>
+        </li>
+      </ul>
+    </aside>
   );
 };
 
-export default Sidebar;
+const Navbar = () => {
+  return (
+    <div className="bg-blue-600 py-2">
+      <div className="container mx-auto flex items-center justify-between">
+        <h1 className="text-white text-xl font-semibold">Your Brand</h1>
+        <div className="hidden lg:flex space-x-4">
+          <a href="#" className="text-white hover:text-blue-200">
+            Category 1
+          </a>
+          <a href="#" className="text-white hover:text-blue-200">
+            Category 2
+          </a>
+          <a href="#" className="text-white hover:text-blue-200">
+            Category 3
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="flex h-screen bg-gray-200 dark:bg-gray-900">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Main Content */}
+      <div className="flex-1">
+        <Navbar />
+        <div className="container mx-auto p-4">
+          {/* Content Goes Here */}
+          <p>Your page content goes here.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
